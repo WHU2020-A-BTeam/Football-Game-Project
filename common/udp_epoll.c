93f3463c1f89923178b27d5f6621125b80e7b844
zhangsheng <zhangsheng@localhost>
2020-07-20 20:41:05 +0800

    zhang push

--- common/udp_epoll.c
+++ common/udp_epoll.c
@@ -11,4 +11,39 @@ void del_event(int epollfd, int fd){
 	return;
 }
 
+int udp_accept(int fd, struct User *user){
+    int new_fd, ret;
+    struct sockaddr_in client;
+    struct LogRequest request;
+    struct LogResponse response;
+    socklen_t len = sizeof(client);
+    
+    bzero(&client, len);
+    bzero(&request, sizeof(request));
+    bzero(&response, sizeof(response));
 
+    ret = recvfrom(fd, (void *)&request, sizeof(request), 0, (struct sockaddr *)&client, &len);
+    
+    if (ret != sizeof(request)){
+        response.type = 1;
+        strcpy(response.msg, "Login failed with Data errors!");
+        sendto(fd, (void *)&response, sizeof(response), 0, (struct sockaddr *)&client, len);
+        return -1;   
+    }
+    
+    new_fd = udp_connect(&client);
+    
+    if (new_fd < 0){
+        return -1;    
+    }
+    strcpy(user->name, request.name);
+    user->team = request.team;
+    user->fd = new_fd;
+   
+    response.type = 0;
+    strcpy(response.msg, "Login Success, Enjoy Yourself");
+    send(new_fd, (void *)&response, sizeof(response), 0);
+    
+    return new_fd; 
+}    
+ 
